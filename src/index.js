e70657828268f4ed7dc8ebc14064758c7a3f5333
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-20 23:48:12 +0530

    Fixed trialDivision

--- src/index.js
+++ src/index.js
@@ -87,7 +87,14 @@ function binarySearch(arr, elem, cmpFn = (a, b) => a - b) {
 */
 
 function trialDivision(number) {
-  return number;
+  if (number < 2) return false;
+  if (number === 2) return true;
+  if (number % 2 === 0) return false;
+
+  for (let i = 3; i * i <= number; i += 2) {
+    if (number % i === 0) return false;
+  }
+  return true;
 }
 
 module.exports = {
